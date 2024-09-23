ed599be4c95a758fb142124d5d31d51cd8580f25
Rajibbepary <rajib635356@gmail.com>
2024-09-23 18:51:12 +0600

    donation button

--- js/index.js
+++ js/index.js
@@ -1,27 +1,49 @@
 
 
+
 document.getElementById('donation-now').addEventListener('click', function(event){
-
-
     event.preventDefault()
-
     const inputValue =parseFloat(document.getElementById('input-value').value);
+    const accountBalance = parseFloat(document.getElementById('account-balance').innerText);
 
-    const accountBalance = parseFloat(document.getElementById('account-balance').innerText)
-
-    console.log(accountBalance)
-
-    if(isNaN(inputValue)){
-        alert('Fail to mony')
+    if(isNaN(inputValue) || inputValue < 0){
+        alert('Invalid Amount Donate')
         return;
     }
 
   const x = document.getElementById('total-donation').innerHTML = inputValue;
 
   const y = accountBalance - x;
-  document.getElementById('account-balance').innerHTML = y;
-   // const totalDonation = accountBalance - totalDonation;
-
-    console.log(inputValue);
+    
+  document.getElementById('account-balance').innerText =  y;
+   
 
 })
+
+
+// history tab function 
+
+const historyTab = document.getElementById('History-btn');
+const donationTab = document.getElementById('donation-btn');
+historyTab.addEventListener('click', function(){
+historyTab.classList.add('bg-[#B4F461]',
+  'focus:outline-none',
+  'transition-colors',
+  'duration-200',
+  'hover:bg-[#B4F461]',
+  'font-semibold',
+  'text-lg'
+  
+  );
+
+donationTab.classList.remove(
+'bg-[#B4F461]',
+'focus:outline-none',
+'transition-colors',
+'duration-200',
+'hover:bg-[#B4F461]',
+'font-semibold'
+);
+
+document.getElementById('parent-container').classList.add('hidden')
+});
