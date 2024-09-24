7c3b9b80fb0c58cd417cce2a9ee781af0c9b24dc
Rajibbepary <rajib635356@gmail.com>
2024-09-24 11:25:25 +0600

    project done

--- js/second.js
+++ js/second.js
@@ -1,3 +1,35 @@
 
+document.getElementById('donation-now-2').addEventListener('click', function(){
 
+const inputValue = parseFloat(document.getElementById('input-value-2').value);
+
+const accountBalance =parseFloat(document.getElementById("account-balance").innerText) 
+
+
+if(isNaN(inputValue) || inputValue < 0){
+    alert('Invalid Amount Donate')
+    return;
+}
+
+const x = document.getElementById('total-donation-2').innerHTML = inputValue;
+
+const y = accountBalance - x;
+    
+  document.getElementById('account-balance').innerText =  y;
+
+  const historyItem = document.createElement("div");
+historyItem.className = "bg-slate-100 drop-shadow-md mt-24 p-10 rounded-md border-l-2 border-r-2 border-black w-9/12 text-center m-auto";
+
+historyItem.innerHTML = `
+
+<h2 class="text-black lg:text-2xl font-semibold">${x} Taka is a Donated for Flood at Noakhali, Bangladesh 2024</h2>
+<p class="mt-5">Date:${new Date()}</p>
+
+`
+const HistoryContainer = document.getElementById("History-list-container");
+HistoryContainer.insertBefore(historyItem, HistoryContainer.firstChild);
+
+
+
+});
 
